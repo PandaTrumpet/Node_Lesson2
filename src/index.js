dbb70e9ca9d6352cf269ab8063b1011ffc45f2fb
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-09 23:07:34 +0300

    updaete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
 const PORT = 3000;
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
 });
 app.listen(PORT, () => {
   console.log(`Server is running om port ${PORT}`);
